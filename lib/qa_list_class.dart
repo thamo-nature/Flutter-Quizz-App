4e1677dda05d9d2d85ea2fa5c99106de779f51bc
thamo-nature <stjmns3@gmail.com>
2022-01-29 02:53:45 +0530

    first commit

---
+++ lib/qa_list_class.dart
@@ -0,0 +1,8 @@
+import 'question_class.dart';
+
+class Qalist {
+  List<Question> questionBank = [
+    Question(q: 'Is reality an illusion ?.', a: true),
+    Question(q: 'Are we been played by universe ?.', a: false),
+  ];
+}
